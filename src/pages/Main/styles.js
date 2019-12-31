5c5ec2a89ebce505b3a3ca2865146fe42ccbc096
pedroccaetano <pedroccaetano@live.com>
2019-12-31 10:52:52 -0300

    Init Main page

--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -1,9 +1,7 @@
 import styled from 'styled-components'
 
-export const Title = styled.h1`
-  font-size: 24px;
-  color: #fff;
-  font-family: Arial, Helvetica, sans-serif;
+export const Container = styled.div``;
 
+export const Form = styled.form``;
 
-`;
+export const SubmitButton = styled.button``;
