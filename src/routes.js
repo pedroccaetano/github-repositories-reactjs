5fa2a3f4206684f90d442644b9ee274493c64197
Pedro Caetano <pedroccaetano@live.com>
2020-01-01 22:47:36 -0300

    Start repository page

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,15 @@
-import React from 'react'
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-import Main from './pages/Main'
-import Repository from './pages/Repository'
+import Main from './pages/Main';
+import Repository from './pages/Repository';
 
 export default function Routes() {
   return (
     <BrowserRouter>
       <Switch>
         <Route path="/" exact component={Main} />
-        <Route path="/repository" exact component={Repository} />
+        <Route path="/repository/:repository" exact component={Repository} />
       </Switch>
     </BrowserRouter>
   );
