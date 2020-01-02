cafcc1a7ad2bf1e71a517b6effe479ad49508726
Pedro Caetano <pedroccaetano@live.com>
2020-01-01 21:12:40 -0300

    Create css to main page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-import Routes from './routes.js'
-import GlobalStyle from './styles/global'
+import Routes from './routes.js';
+import GlobalStyle from './styles/global';
 
 function App() {
   return (
